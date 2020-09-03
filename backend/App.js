eb9282cd5857f906357df69dc3f26dee533b8756
DanielHansson <dh99@live.se>
2020-09-03 13:23:12 +0200

    Recieved data from Willys to Backend
    Using Chicken categories and frozen chicken

--- backend/App.js
+++ backend/App.js
@@ -2,12 +2,23 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.json());
 app.get('/', (req, res) => {
   res.send('Hello World! From Express!')
 })
 
 const TestHarvesting = require("./Harvesters/MainHarvesterFactory");
-TestHarvesting.test();
+/* TestHarvesting.test(); */
+
+app.get('/test', (req, res) => {
+  TestHarvesting.test().then(result => {
+    res.status(300).json(result);
+  }).then(console.log("Success!!"))
+    .catch(err => {
+    console.error(err)
+  })
+}) //Pushed information from Willys to backend, printing Scrubbed products
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
