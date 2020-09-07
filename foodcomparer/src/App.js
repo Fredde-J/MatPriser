5a4d844039efb9e8b2189d27a88bb30ca01ddc0d
Fredrik Jönsson <fredrikjay@gmail.com>
2020-09-07 10:21:30 +0200

    added start page and routing from app to startpage

--- foodcomparer/src/App.js
+++ foodcomparer/src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import './App.css';
+import StartPage from './pages/StartPage'
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World from App</h1>
+      <h1>Header</h1>
+      <BrowserRouter>
+      <main className="container">
+        <Switch>
+          <Route exact path="/" component={StartPage} />
+        </Switch>
+
+      </main>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ foodcomparer/src/pages/StartPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { withRouter } from "react-router-dom";
+
+
+const StartPage = ()=>{
+    return (
+        <>
+        <h1>start page</h1>
+        </>
+        )
+}
+
+
+    export default withRouter(StartPage);
