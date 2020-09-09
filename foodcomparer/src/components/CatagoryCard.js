d74c810b06e644cbfb42a8a293855f68445f0890
Fredrik Jönsson <fredrikjay@gmail.com>
2020-09-09 15:38:34 +0200

    center align items in cards with bootstrap

--- foodcomparer/src/components/CatagoryCard.js
+++ foodcomparer/src/components/CatagoryCard.js
@@ -11,11 +11,11 @@ import {
 const CatagoryCard = ()=>{
     return (
         <>
-        <Card className="col-5 ml-4 mb-3 ">
+        <Card className="col-5 ml-4 mb-3 d-flex flex-wrap align-items-center ">
         <CardBody>
           <h3>Lätt Mjölk</h3>
         </CardBody>
-        <img height="160vh" width="160vw" src={milk} alt="Card image cap" />
+        <img height="150vh" width="150vw" src={milk} alt="Card image cap" />
       </Card>
         </>
         )
