461b6a7305ac5f02eb700c99fecf44716e6b94a4
Fredrik Jönsson <fredrikjay@gmail.com>
2020-09-16 14:28:19 +0200

    added routing to startPage in header

--- foodcomparer/src/components/Header.js
+++ foodcomparer/src/components/Header.js
@@ -3,12 +3,15 @@ import { withRouter } from "react-router-dom";
 import listIcon from "../images/listIcon.svg";
 import { Row, Col } from "reactstrap";
 
-const Header = () => {
+const Header = (props) => {
+  const goTohomepage =()=>{
+    props.history.push("/")
+  }
   return (
     <>
       <Row className="" style={{ backgroundColor: "rgb(250, 246, 184)" }}>
         <Col className="col-11 ">
-          <h1 style={{ fontSize: "3em" }}>SnålKöp</h1>
+          <h1 style={{ fontSize: "3em" }} onClick={goTohomepage}>SnålKöp</h1>
         </Col>
         <Col className="col-1">
           <img
