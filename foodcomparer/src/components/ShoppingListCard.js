d0afb2df9dcb4dec6ff7979c0fd056c235f20259
Fredrik Jönsson <fredrikjay@gmail.com>
2020-09-16 14:40:30 +0200

    added shoppingListCard in componets and add it to page

---
+++ foodcomparer/src/components/ShoppingListCard.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const ShoppingListCard = ()=>{
+return(
+    <>
+    <p>im a card</p>
+    </>
+)
+}
+export default ShoppingListCard
--- foodcomparer/src/pages/ShoppingListPage.js
+++ foodcomparer/src/pages/ShoppingListPage.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { withRouter } from "react-router-dom";
+import ShoppingListCard from "../components/ShoppingListCard"
 
 const ShoppingListPage =()=>{
  return (
      <>
      <h1>shopping list</h1>
+     <ShoppingListCard></ShoppingListCard>
      </>
  )
 
