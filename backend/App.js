701dde6b48b27741770c41ee4ee3cc3b13b1166e
Aniko Magyar <mofolade@gmail.com>
2020-09-16 13:20:05 +0200

    changed port

--- backend/App.js
+++ backend/App.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 4000;
 const APIManager = require("./APIManager");
 const HarvesterFactory = require("./Harvesters/MainHarvesterFactory");
 
