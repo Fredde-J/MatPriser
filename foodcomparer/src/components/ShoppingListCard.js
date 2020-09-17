3ee99bcd403a0d278259e0154ad519c19bcea906
Fredrik Jönsson <fredrikjay@gmail.com>
2020-09-17 16:26:06 +0200

    total price from the different stores now shows in the cards, code clean

--- foodcomparer/src/components/ShoppingListCard.js
+++ foodcomparer/src/components/ShoppingListCard.js
@@ -1,89 +1,89 @@
-import React, {useState, useEffect, useContext } from "react";
-import {
-    Card,
-    Row,
-    Col
-  } from "reactstrap";
-  import willys from '../images/willys.jpg'
-  import coop from '../images/coop4.png'
-  import hemkop from '../images/hemkop.jpg'
-  import {ProductContext} from '../ContextProviders/ProductContextProvider';
+import React, { useState, useEffect, useContext } from "react";
+import { Card, Row, Col } from "reactstrap";
+import willys from "../images/willys.jpg";
+import coop from "../images/coop4.png";
+import hemkop from "../images/hemkop.jpg";
+import { ProductContext } from "../ContextProviders/ProductContextProvider";
 
-const ShoppingListCard = ()=>{
-    let allProducts = useContext(ProductContext)
-    const [totalPrice, setTotalPrice] = useState(0)
-    
+const ShoppingListCard = () => {
+  let allProducts = useContext(ProductContext);
+  const [coopTotalPrice, setcoopTotalPrice] = useState(0);
+  const [willysTotalPrice, setwillysTotalPrice] = useState(0);
+  const [hemkopTotalPrice, sethemkopTotalPrice] = useState(0);
 
-    if(allProducts.products[0] !=undefined){
-        localStorage.clear()
-        let shoppingItems = [];
-        shoppingItems.push(allProducts.products[0],allProducts.products[1],allProducts.products[3])
-        console.log(shoppingItems)
-        localStorage.setItem("shoppingList", JSON.stringify(shoppingItems))
-    }
-        
-    
+  //the if below will be removed when product card is complete 
+  if (allProducts.products[0] != undefined) {
+    let shoppingItems = [];
+    shoppingItems.push(
+      allProducts.products[0],
+      allProducts.products[1],
+      allProducts.products[7]
+    );
+    localStorage.setItem("shoppingList", JSON.stringify(shoppingItems));
+  }
 
-    const getPrice = () =>{
-        
-        let itemsFromLocalStorage = JSON.parse(localStorage.getItem("shoppingList"))
-        console.log(itemsFromLocalStorage)
+  const getPrice = () => {
+    let itemsFromLocalStorage = JSON.parse(
+      localStorage.getItem("shoppingList")
+    );
+    console.log(itemsFromLocalStorage);
+    let willysPrices = 0;
+    let coopPrices = 0;
+    let hemkopPrices = 0;
 
-        let price = 0;
-    
-        itemsFromLocalStorage.forEach(item =>price = price + item.pricePerItem )
-        console.log(price)
-        setTotalPrice(price)
-    }
+    itemsFromLocalStorage.forEach(async (item) => {
+      if (item.storeId == 0) {
+        willysPrices += item.pricePerItem;
+      } else if (item.storeId == 1) {
+        coopPrices += item.pricePerItem;
+      } else if (item.storeId == 2) {
+        hemkopPrices += item.pricePerItem;
+      }
+    });
+    setcoopTotalPrice(coopPrices.toFixed(2));
+    setwillysTotalPrice(willysPrices.toFixed(2));
+    sethemkopTotalPrice(hemkopPrices.toFixed(2));
+  };
 
-    useEffect(()=>{
-        getPrice()
-        console.log(totalPrice)
-    },[allProducts])
+  useEffect(() => {
+    getPrice();
+  }, [allProducts]);
 
- 
-return(
+  return (
     <>
-
-        <Card body >
-            <Row>
-            <Col xs="6">
-            <img  src={willys} height="100vh" width="150vw" alt="affär"></img>
-            </Col>
-            <Col xs="6" >
-          <h2 className="text-right mt-4">528 kr</h2>
+      <Card body>
+        <Row>
+          <Col xs="6">
+            <img src={willys} height="100vh" width="150vw" alt="affär"></img>
           </Col>
-          
-          </Row>
-        </Card>
-
-        <Card body >
-            <Row>
-            <Col xs="6">
-            <img  src={coop} height="100vh" width="150vw" alt="affär"></img>
-            </Col>
-            <Col xs="6" >
-         <h2 className="text-right mt-3">{totalPrice} kr</h2>
+          <Col xs="6">
+            <h2 className="text-right mt-4">{willysTotalPrice} kr</h2>
           </Col>
-          
-          </Row>
-        </Card>
+        </Row>
+      </Card>
 
-        <Card body >
-            <Row>
-            <Col xs="6">
-            <img  src={hemkop} height="100 vh" width= "150vw" alt="affär"></img>
-            </Col>
-            <Col xs="6" >
-          <h2 className="text-right mt-3">528 kr</h2>
+      <Card body>
+        <Row>
+          <Col xs="6">
+            <img src={coop} height="100vh" width="150vw" alt="affär"></img>
           </Col>
-          
-          </Row>
-        </Card>
+          <Col xs="6">
+            <h2 className="text-right mt-3">{coopTotalPrice} kr</h2>
+          </Col>
+        </Row>
+      </Card>
 
-     
-      
+      <Card body>
+        <Row>
+          <Col xs="6">
+            <img src={hemkop} height="100 vh" width="150vw" alt="affär"></img>
+          </Col>
+          <Col xs="6">
+            <h2 className="text-right mt-3">{hemkopTotalPrice} kr</h2>
+          </Col>
+        </Row>
+      </Card>
     </>
-)
-}
-export default ShoppingListCard
+  );
+};
+export default ShoppingListCard;
