0a3ab7c6daee95dea9139b1c023bfcef43c3ce96
Aniko Magyar <mofolade@gmail.com>
2020-09-23 20:21:02 +0200

    new field in country table (nameEng), new data

--- backend/db/mat_pris_country.sql
+++ backend/db/mat_pris_country.sql
@@ -24,9 +24,10 @@ DROP TABLE IF EXISTS `country`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `country` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(45) DEFAULT NULL,
+  `name` varchar(30) DEFAULT NULL,
+  `nameEng` varchar(30) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=35 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=46 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -35,7 +36,7 @@ CREATE TABLE `country` (
 
 LOCK TABLES `country` WRITE;
 /*!40000 ALTER TABLE `country` DISABLE KEYS */;
-INSERT INTO `country` VALUES (1,'Belgien'),(2,'Brasilien'),(3,'Chile'),(4,'Colombia'),(5,'Costa Rica'),(6,'Ecuador'),(7,'Egypten'),(8,'Elfenbenskusten'),(9,'Frankrike'),(10,'Holland'),(11,'Iran'),(12,'Israel'),(13,'Italien'),(14,'Kina'),(15,'Nederländerna'),(16,'Nya Zeeland'),(17,'Polen'),(18,'Serbien'),(19,'Spanien'),(20,'Sverige'),(21,'Sydafrika'),(22,'Turkiet'),(23,'Ungern'),(24,'Tyskland'),(25,'Tjeckien'),(26,'Danmark'),(27,'Österrike'),(28,'Estland'),(29,'Finland'),(30,'Japan'),(31,'Kroatien'),(32,'Norge'),(33,'USA'),(34,'Storbritannien');
+INSERT INTO `country` VALUES (1,'Belgien','Belgium'),(2,'Brasilien','Brazil'),(3,'Chile','Chile'),(4,'Colombia','Colombia'),(5,'Costa Rica','Costa Rica'),(6,'Ecuador','Ecuador'),(7,'Egypten','Egypt'),(8,'Elfenbenskusten','Ivory Coast'),(9,'Frankrike','France'),(10,'Nederländerna','Holland'),(11,'Iran','Iran'),(12,'Israel','Israel'),(13,'Italien','Italy'),(14,'Kina','Kina'),(16,'Nya Zeeland','New Zealand'),(17,'Polen','Poland'),(18,'Serbien','Serbia'),(19,'Spanien','Spain'),(20,'Sverige','Sweden'),(21,'Sydafrika','South Africa'),(22,'Turkiet','Turkey'),(23,'Ungern','Hungary'),(24,'Tyskland','Germany'),(25,'Tjeckien','Czech Republic'),(26,'Danmark','Denmark'),(27,'Österrike','Austria'),(28,'Estland','Estonia'),(29,'Finland','Finland'),(30,'Japan','Japan'),(31,'Kroatien','Croatia'),(32,'Norge','Norway'),(33,'USA','UK'),(34,'Storbritannien','UK'),(35,'Australien','Australia'),(36,'Tunisien','Tunisia'),(37,'Kenya','Kenya'),(38,'Peru','Peru'),(39,'Portugal','Portugal'),(40,'Zimbabwe','Zimbabwe'),(41,'Mexico','Mexico'),(42,'Litauen','Lithuania'),(43,'Schweiz','Switzerland'),(44,'Kazakstan','Kazakstan'),(45,'Malaysia','Malaysia');
 /*!40000 ALTER TABLE `country` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -48,4 +49,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-09-23 16:58:06
+-- Dump completed on 2020-09-23 20:20:08
