8eb848def58f097c4ca0be92cc059bca1fea0540
Fredrik Jönsson <fredrikjay@gmail.com>
2020-09-28 14:26:38 +0200

    you can now add similarProducts to localstore

--- foodcomparer/src/ContextProviders/ProductContextProvider.js
+++ foodcomparer/src/ContextProviders/ProductContextProvider.js
@@ -7,6 +7,7 @@ const ProductContextProvider = (props) => {
   const [products, setProducts] = useState([]);
   const [mainCatProducts, setMainCatProducts] = useState([]);
   const [store, setStore] = useState([]);
+  const [similarProducts, setSimilarProducts] = useState([])
   
   const getProducts = async () => {
     axios
@@ -36,19 +37,20 @@ const ProductContextProvider = (props) => {
     });
   }
 
-  const getSimilarProductsById = async (id) => {
-    axios
-    .get("" + id)
-    .then((response) => {
+  const getSimilarProducts = async () => {
+    let error ;
+    var result = await axios.get("http://localhost:4000/rest/similareproducts")
+      .catch(e => error = e);
+    return result.data || {error};
+    /*.then((response) => {
+      console.log("GOT IT")
       return response.data;
     }).then((result) => {
-      setStore(result)
-      console.log(result)
-      console.log("store", result)
+      setSimilarProducts(result)
     })
-    .catch((err) => {
+     .catch((err) => {
       console.error(err);
-    });
+    });*/
   }
 
 
@@ -70,16 +72,16 @@ const ProductContextProvider = (props) => {
 
   useEffect(() => {
     getProducts();
-    getProductsByMainCatId();
   }, []);
 
   const values = {
     products,
     store, 
     mainCatProducts,
+    similarProducts,
     getProductsByMainCatId,
     getStoreNameById,
-    getSimilarProductsById,
+    getSimilarProducts,
   };
   return (
     <ProductContext.Provider value={values}>
