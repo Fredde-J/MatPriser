b7db7cfc1fa96b74d5eba2de2414d317bf89c0e5
DanielHansson <dh99@live.se>
2020-10-07 11:32:12 +0200

    Changed position on - and + onAdd/remove buttons

--- foodcomparer/src/components/ShoppingListProductCard.js
+++ foodcomparer/src/components/ShoppingListProductCard.js
@@ -21,9 +21,9 @@ const ShoppingListProductCard = (props) => {
           </Col>
           <Col xs="2">
             <ButtonGroup size="sm">
-              <Button onClick={onAddClick}>+</Button>
-              <Button disabled>{props.product.amount}</Button>
               <Button onClick={onRemoveClick}>-</Button>
+              <Button disabled>{props.product.amount}</Button>
+              <Button onClick={onAddClick}>+</Button>
             </ButtonGroup>
           </Col>
           <Col xs="2" style={{ color: false ? "red" : null }}>
